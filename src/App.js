077ee6824d5bb160303548e7a024484ba4935731
Abdullah <70604820+abdullah-25@users.noreply.github.com>
2023-01-21 23:57:15 -0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import { MacBookAirRootRootRoot1 } from "./MacBookAirRootRootRoot1";
+import { SecondPage } from "./SecondPage";
+
+export default function App() {
+  document.body.style.backgroundColor = "#1e1e1e";
+  document.body.style.margin = "0";
+  return (
+    <>
+      <MacBookAirRootRootRoot1 event={""} date={""} information={""} />
+      <SecondPage
+        Revent1={""}
+        Rdate1={""}
+        Rinfo1={""}
+        Revent2={""}
+        Rdate2={""}
+        Rinfo2={""}
+        Revent3={""}
+        Rdate3={""}
+        Rinfo3={""}
+        Sevent1={""}
+        Sdate1={""}
+        Sinfo1={""}
+        Sevent2={""}
+        Sdate2={""}
+        Sinfo2={""}
+      />
+    </>
+  );
+}
